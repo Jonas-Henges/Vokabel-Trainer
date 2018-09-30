13576d00fcc5a22721651f06a07bd60ec59355ab
Jonas <jonas.henges@t-online.de>
2018-09-30 14:25:53 +0200

    Initial commit

---
+++ src/Vokabel.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class Vokabel {
+
+    private String deutsch;
+    private String englisch;
+    private Date nextdate;
+
+    public Vokabel(String deutsch, String englisch, Date nextdate) {
+
+        this.deutsch = deutsch;
+    }
+}
